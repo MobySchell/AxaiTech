fc1665a8af35c47cae60e6d40fac31b9dbafe16c
nzaya1 <86688312+nzaya1@users.noreply.github.com>
2021-08-10 15:27:35 -0400

    updated nodemailer on doctor-register page

--- src/components/login/DoctorRegister.js
+++ src/components/login/DoctorRegister.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import firebase from "../../firebase/firebase";
+import axios from "axios";
 
 export default class DoctorRegister extends Component {
     constructor(props) {
@@ -53,12 +54,51 @@ export default class DoctorRegister extends Component {
         }
     }
 
+    handleSubmit(e) {
+        this.register(e);
+        e.preventDefault();
+       // const firstName = document.getElementById("name").value;
+       // const email = document.getElementById("email").value;
+       // const hpcsa = document.getElementById("hpcsa").value;
+
+       const firstName = this.state.firstName;
+       const email = this.state.email;
+       const hpcsa = this.state.hpcsa;
+     
+     
+     
+        axios({
+          method: "POST",
+          url: "http://localhost:3000/doctorregister",
+          data: {
+            name : firstName,
+            email: email,
+            hpcsa: hpcsa,
+          },
+        }).then((response) => {
+          if (response.data.msg === "success") {
+            alert("Message Sent.");
+            this.resetForm();
+          } else if (response.data.msg === "fail") {
+            alert("Message failed to send.");
+          }
+        });
+      }
+     
+     
+     
+      resetForm() {
+        document.getElementById("registerForm").reset();
+      }
+     
+     
+
     render() {
         return (
             <div className="container col-7 mt-2">
                 <div className="p-5"></div>
                 <div className="card card-body text-center">
-                    <form onSubmit={(e) => this.register(e)}>
+                    <form id="registerForm" onSubmit={(e) => this.handleSubmit(e)}>
                         <h1 className="h3 mt-3 text-center">Please Register</h1>
                         <div className="p-3 body">
                             <input
@@ -71,6 +111,7 @@ export default class DoctorRegister extends Component {
                                 type="text"
                                 className="form-control"
                                 placeholder="First Name"
+                                id= "name"
                             />
                         </div>
                         <div className="p-3 body">
