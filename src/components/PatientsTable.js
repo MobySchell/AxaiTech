d6ac18277ffd698aa21785d849c1eabc0885ecd6
peter-jankowski <pjankowski@college.harvard.edu>
2021-08-12 23:58:51 -0700

    combined pj and jo code for doctor portal

--- src/components/PatientsTable.js
+++ src/components/PatientsTable.js
@@ -3,6 +3,8 @@ import { Table } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import firebase from "../firebase/firebase";
 import { Link } from "react-router-dom";
+import AddImg from "../components/AddImg";
+import AddPatient from "./AddPatient";
 
 export default class PatientsTable extends Component {
   constructor(props) {
@@ -10,7 +12,8 @@ export default class PatientsTable extends Component {
     this.handleClick = this.handleClick.bind(this);
     this.state = {
       patients: [],
-      selectedId: null
+      selectedId: null,
+      report: "https://firebasestorage.googleapis.com/v0/b/axai-tech.appspot.com/o/reports%2FAxaitech_Med_v2.pdf?alt=media&token=0def6eaa-6a51-49c5-a28b-d3b04e2fd133"
     };
 
     this.db = firebase.firestore();
@@ -28,6 +31,9 @@ export default class PatientsTable extends Component {
     return (
       <div className="p-2">
         <h1 className="text-center pb-5">List of Patients</h1>
+                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                              Add Patient
+                            </button>
         <Table striped bordered hover variant="light">
           <thead>
             <tr>
@@ -90,6 +96,15 @@ export default class PatientsTable extends Component {
                                                 <td>{test.diagnosis}</td>
                                                 <td>{date}</td>
                                                 <td>{test.status}</td>
+                                                <td>
+                                                    <a target="_blank" href= {this.state.report} rel="noreferrer">
+                                                        <button type="button" className="btn btn-success m-1">View</button>
+                                                    </a>
+                                                </td>
+                                                <td>
+                                                    <button type="button" className="btn btn-warning m-1" data-bs-toggle="modal" data-bs-target="#exampleModal2">Share
+                                                    </button>
+                                                </td>
                                         </tr>
                                      );
                                 })}
@@ -102,7 +117,42 @@ export default class PatientsTable extends Component {
             })}
           </tbody>
         </Table>
-      </div>
-    );
+                          
+
+                            <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                              <div className="modal-dialog">
+                                <div className="modal-content">
+                                  <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">Doctor</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                  </div>
+                                  <div className="modal-body">
+                                    <AddPatient />
+                                  </div>
+                                  <div className="modal-footer">
+                                    {/* <button type="button" className="btn btn-primary">Send report</button> */}
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                        <div className="modal fade" id="exampleModal2" aria-hidden="true">
+                          <div className="modal-dialog">
+                            <div className="modal-content">
+                              <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Doctor</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                              </div>
+                              <div className="modal-body">
+                                <AddPatient />
+                                
+                              </div>
+                              <div className="modal-footer">
+                                {/* <button type="button" className="btn btn-primary">Send report</button> */}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                     );
   }
 }
